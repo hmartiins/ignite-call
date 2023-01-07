144bf5c1ed3afefe18b53da6111729f93c6ba38b
hmartiins <hmartins224@gmail.com>
2023-01-07 12:55:44 -0300

    create home page

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+
+import { Heading, Text } from "@ignite-ui/react";
+import { Container, Hero, Preview } from "./styles";
+
+import previewImage from "../../assets/appPreview.png";
+import { ClaimUsernameForm } from "./components/ClaimUsernameForm";
+
+export default function Home() {
+  return (
+    <Container>
+      <Hero>
+        <Heading size={"4xl"}>Agendamento Descomplicado</Heading>
+        <Text size={"xl"}>
+          Conecte seu calendário e permita que as pessoas marquem agendamentos
+          no seu tempo livre.
+        </Text>
+
+        <ClaimUsernameForm />
+      </Hero>
+
+      <Preview>
+        <Image
+          src={previewImage}
+          height={400}
+          quality={100}
+          priority
+          alt="Calendário simbolizando a aplicação em execução"
+        />
+      </Preview>
+    </Container>
+  );
+}
